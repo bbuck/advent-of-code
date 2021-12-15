c1d4f363e38b269bddc9d976b4f5bb501c9580c8
Brandon Buck <lordizuriel@gmail.com>
2021-12-14 22:52:32 -0600

    start day 2

---
+++ 2021/kotlin/me/brandonbuck/adventofcode/dive/Solution1.kt
@@ -0,0 +1,18 @@
+// Copyright 2021 Brandon Buck
+
+// Advent of Code 2021 "Dive!"
+// Day 2, Puzzle 1
+
+package me.brandonbuck.adventofcode.dive
+
+import me.brandonbuck.adventofcode.common.readAllFromStandardIn
+
+fun main() {
+	val inputs = readAllFromStandardIn()
+
+	println(solution1(inputs))
+}
+
+fun solution1(inputs: List<String>): Int {
+	return inputs.size
+}
